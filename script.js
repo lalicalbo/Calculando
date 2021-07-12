115f5d182ca2956640fdb12f8f9413ed80352c52
laura <ll.calderon14@gmail.com>
2021-07-12 17:05:44 -0500

    Realizando calculos con html y javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+let num1=parseFloat(prompt(`Indique primer numero `));
+let num2=parseFloat(prompt(`Indique segundo numero `));
+let num3=parseFloat(prompt(`Indique tercer numero`));
+
+let result= alert("la suma de los numeros es " + (num1+num2+num3));
